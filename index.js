4ed4bc8c436b9498bd60514d5d955ca1a54f6bff
SUDARSHAN NATH MAZUMDAR <sudarshannathmazumdar@gmail.com>
2025-07-12 18:03:41 +0530

    hosting serverless backend

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const mongoose = require("mongoose");
+const serverless = require("serverless-http");
 
 const authRouter = require("./routes/authRoutes");
 const clinicRouter = require("./routes/clinicRoutes");
@@ -14,16 +15,33 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 
-mongoose
-  .connect(process.env.MONGODB_URI, {
-    dbName: process.env.MONGODB_DB,
-  })
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((err) => {
-    console.log("Error connecting to MongoDB", err);
-  });
+// mongoose
+//   .connect(process.env.MONGODB_URI, {
+//     dbName: process.env.MONGODB_DB,
+//   })
+//   .then(() => {
+//     console.log("Connected to MongoDB");
+//   })
+//   .catch((err) => {
+//     console.log("Error connecting to MongoDB", err);
+//   });
+
+// MongoDB connection (runs once)
+let isDbConnected = false;
+async function connectDb() {
+  if (!isDbConnected) {
+    try {
+      await mongoose.connect(process.env.MONGODB_URI, {
+        dbName: process.env.MONGODB_DB,
+      });
+      isDbConnected = true;
+      console.log("✅ Connected to MongoDB");
+    } catch (err) {
+      console.log("❌ Error connecting to MongoDB", err);
+    }
+  }
+}
+connectDb();
 
 app.get("/", (req, res) => {
   res.status(200).json({
@@ -37,6 +55,9 @@ app.use("/user", userRouter);
 app.use("/admin", adminRouter);
 app.use("/user/webhook/appointments", userRouter);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server is running on http://localhost:${PORT}`);
+// });
+
+// ✅ Export serverless handler
+module.exports = serverless(app);
